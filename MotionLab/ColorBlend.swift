a0598185db2eeff92c3c0fcbb0ed1db78347c851
Drew Crawford <drew@sealedabstract.com>
2014-08-20 17:27:19 -0500

    Progressive animations

---
+++ MotionLab/ColorBlend.swift
@@ -0,0 +1,24 @@
+//
+//  ColorBlend.swift
+//  MotionLab
+//
+//  Created by Drew Crawford on 8/20/14.
+//  Copyright (c) 2014 DrewCrawfordApps. All rights reserved.
+//
+
+import Foundation
+import UIKit
+func blend(c1: UIColor, c2: UIColor, var alpha: CGFloat) -> UIColor {
+    alpha = min(1.0, max(0, alpha))
+    let beta = 1 - alpha;
+    var r1 = CGFloat(0), g1 = CGFloat(0), b1 = CGFloat(0), a1 = CGFloat(0), r2 = CGFloat(0), g2 = CGFloat(0), b2 = CGFloat(0), a2 = CGFloat(0)
+    
+    c1.getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+    c2.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+    let r = r1 * beta + r2 * alpha
+    let g = g1 * beta + g2 * alpha
+    let b = b1 * beta + b2 * alpha
+    return UIColor(red: r, green: g, blue: b, alpha: 1)
+    
+    
+}
---
+++ MotionLab/ProgressiveAnimationVC.swift
@@ -0,0 +1,45 @@
+//
+//  SecondViewController.swift
+//  MotionLab
+//
+//  Created by Drew Crawford on 8/20/14.
+//  Copyright (c) 2014 DrewCrawfordApps. All rights reserved.
+//
+
+import UIKit
+
+class ProgressiveAnimationVC: UIViewController {
+                            
+    @IBOutlet var manipulable: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        manipulable.backgroundColor = colorForProgress(0.0)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func gestureRecognizer(sender: UIPanGestureRecognizer) {
+        manipulable.center = sender.locationInView(self.view);
+        
+        let progress = sender.locationInView(self.view).y / self.view.bounds.height;
+        manipulable.backgroundColor = colorForProgress(progress);
+        
+        
+    }
+    
+    func colorForProgress(progress: CGFloat) -> UIColor {
+        let startColor = UIColor(red: 0.3, green: 0.5, blue: 0.8, alpha: 1.0)
+        let endColor = UIColor(red: 0.5, green: 0.8, blue: 0.2, alpha: 1.0)
+        let mixedColor = blend(startColor, endColor, progress)
+        return mixedColor;
+    }
+    
+    
+
+}
+
